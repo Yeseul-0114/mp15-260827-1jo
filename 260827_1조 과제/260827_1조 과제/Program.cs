a60314b1fde2d367d392f1cebec0023c8bd6563d
YeSeul <ths0114@naver.com>
2026-08-27 18:03:43 +0900

    [Feat] 손예슬 소개 메서드 추가

--- 260827_1조 과제/260827_1조 과제/Program.cs
+++ 260827_1조 과제/260827_1조 과제/Program.cs
@@ -4,11 +4,17 @@ public class Program
 {  
     public static void Main()  
     {  
-        Console.WriteLine("=== N조 팀원 소개 ===");  
+        Console.WriteLine("=== 1조 팀원 소개 ===");  
         // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        
+        yeSeul();
 
         Console.WriteLine("=== 소개 끝 ===");  
     }
 
     // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void yeSeul()
+    {
+        Console.WriteLine("안녕하세요, 메플 15기 훈련생 1조 손예슬입니다.");
+    }
 }
